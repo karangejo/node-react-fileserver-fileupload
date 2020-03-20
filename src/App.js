e13848eb5ebf36f548dcf58bda2bb0b1950fbf29
karang <gabriel.g.gras@gmail.com>
2020-03-20 11:49:17 +0800

    basic functionality for server and frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Alert from '@material-ui/lab/Alert';
+import axios from  'axios';
+
 
 function App() {
+
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  const uploadFile = (event) => {
+    console.log("selected");
+    console.log(file);
+    const formData = new FormData();
+    formData.append('uploadedFile',file)
+    const config = {
+       headers: {
+           'content-type': 'multipart/form-data'
+       }
+     }
+    axios.post('http://localhost:3030/upload-file', formData, config)
+      .then(() => {
+        console.log("file Saved");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  const changedFile = (event) => {
+    console.log(event.target.files[0]);
+    setFile(event.target.files[0]);
+    setFileName(event.target.files[0].name);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column" justify="center">
+      <Grid container direction="row" >
+        <input
+            onChange={changedFile}
+            accept="image/*"
+            style={{ display: 'none' }}
+            id="raised-button-file"
+            multiple
+            type="file"
+          />
+          <label htmlFor="raised-button-file">
+            <Button variant="contained" color="primary" component="span" >
+              Choose
+            </Button>
+          </label>
+          {fileName ? <Alert severity="success">{fileName}</Alert> : null}
+        </Grid>
+        <Button variant="contained" color="primary" component="span" onClick={uploadFile}>
+          Upload
+        </Button>
+    </Grid>
   );
 }
+
 
 export default App;
